4a44de40ef4d45b8118dbe22e9ebd0823414f8a1
Durgaprasad Singh <dp.9.singh@gmail.com>
2022-01-21 02:15:31 +0530

    Hello Harry

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, makeStyles, Modal, TextField, Typography } from '@material-ui/core';
+import { Box, Button, Card, CardActions, CardContent, Grid, makeStyles, Modal, TextField, Typography } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 
 const useStyle = makeStyles((theme) => ({
@@ -25,7 +25,7 @@ const useStyle = makeStyles((theme) => ({
     right: {
         backgroundColor: theme.palette.secondary.main,
         display: "flex",
-        flexDirection: "column",
+        flexWrap: "wrap",
         alignItems: "center",
         justifyContent: "center"
     },
@@ -52,11 +52,8 @@ const useStyle = makeStyles((theme) => ({
         alignItems: "center",
         justifyContent: "center"
     },
-    bocContainer: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center"
+    card: {
+        margin: theme.spacing(1),
     },
     payMoney: {
         width: "400px",
@@ -78,66 +75,78 @@ const useStyle = makeStyles((theme) => ({
     }
 }))
 
-type DataState = {
-    id: number,
-    car: string,
-    created_at: number
-}
-
-function getData() {
-    if (localStorage.getItem('data')) {
-        return JSON.parse(localStorage.getItem('data') || "")
-    } else {
-        return []
-    }
-}
 const Home = () => {
     const classes = useStyle();
     const [carNumber, setCarNumber] = useState<string>("");
-    const [store, setStore] = useState<Array<DataState>>(getData());
-    const [updateStore, setUpdateStore] = useState(false);
+    const [slot, setSlot] = useState<any>(null);
+    const [hideSlot, setHideSlot] = useState<boolean>(true);
+    const [numberOfParkingSlot, setNumberOfParkingSlot] = useState<Array<number>>([]);
+    const [blankSpace, setBlankSpace] = useState<Array<number>>([]);
+    const [storeSpace, setstoreSpace] = useState<Array<number>>([]);
     const [message, setMessage] = useState(false);
     const [payment, setPayment] = useState<number | null>(null);
-    const [exitData, setExitData] = useState<DataState | any>(null);
+    const [amountPopup, setAmountPopup] = useState<boolean>(false);
     const [exit, setExit] = useState(false);
+    const [id, setId] = useState<any>(null);
     const [user, setUser] = useState(false);
-    const [hour, setHour] = useState<number>(0);
+    const [hour, setHour] = useState<any>(null);
     const [amount, setAmount] = useState<number>(10)
 
     const addNewCar = () => {
-        if (store.length < 5) {
-            const newData: DataState = {
-                id: store.length + 1,
-                car: carNumber,
-                created_at: new Date().getHours()
-            }
-            setUpdateStore(true);
-            setStore([...store, newData]);
-            setCarNumber("");
+        if (blankSpace.length > 0) {
+            const getRandomValue = Math.floor(Math.random() * blankSpace.length);
+            const text: any = document.getElementById(`carCard${blankSpace[getRandomValue]}`);
+            text.innerHTML = carNumber;
+            console.log(blankSpace);
+            console.log(storeSpace);
+            const newBlankValue = blankSpace.filter((item) => {
+                return item !== blankSpace[getRandomValue]
+            })
+            setstoreSpace((storeSpace) => [...storeSpace, blankSpace[getRandomValue]]);
+            setBlankSpace(newBlankValue);
         } else {
             setMessage(true);
             setCarNumber("");
         }
     }
 
+    const slotArray: number[] = [];
+
+    function addNewSlot() {
+        setHideSlot(false);
+        let i: any = 0;
+        while (i < slot) {
+            slotArray.push(i);
+            i++;
+        }
+        setNumberOfParkingSlot(slotArray);
+        setBlankSpace(slotArray);
+    }
+
     function handleClick(x: number) {
-        const newData = store.find((item) => {
-            return item.id === x
-        });
-        const getCurrHour = new Date().getHours();
-        setHour(getCurrHour);
-        setExitData(newData);
-        setUpdateStore(true);
+        if (document.getElementById(`carCard${x}`)?.innerHTML) {
+            setAmountPopup(true);
+            setId(x);
+        }
+    }
+
+    function getTotalHour() {
+        setAmount(hour > 2 ? (hour - 1) * 10 : 10);
+        setAmountPopup(false);
         setExit(true);
-        setUser(false);
+        setHour(null);
     }
 
     function exitParking() {
         if (payment === amount) {
-            const remove = store.filter((item) => {
-                return item.id !== exitData.id
-            });
-            setStore(remove);
+            const text: any = document.getElementById(`carCard${numberOfParkingSlot[id]}`);
+            text.innerHTML = "";
+            console.log(storeSpace);
+            const newStoreSpace = storeSpace.filter((item) => {
+                return item !== numberOfParkingSlot[id]
+            })
+            setBlankSpace((blankSpace) => [...blankSpace, numberOfParkingSlot[id]]);
+            setstoreSpace(newStoreSpace);
             setPayment(null);
             setExit(false);
         } else {
@@ -145,46 +154,25 @@ const Home = () => {
         }
     }
 
-    useEffect(() => {
-        if (updateStore) {
-            if (hour - exitData.created_at < 2) {
-                setAmount(10);
-            } else {
-                setAmount(((hour - exitData.created_at)-2) * 10 + 10);
-            }
-        }
-        return () => {
-            setUpdateStore(false);
-        }
-    }, [hour])
-
-    useEffect(() => {
-        if (updateStore) {
-            localStorage.setItem('data', JSON.stringify(store));
-        }
-        return () => {
-            setUpdateStore(false);
-        }
-    }, [store]);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setMessage(false);
-        }, 5000);
-        return () => {
-            clearTimeout(timer);
-        }
-    }, [message])
-
     return (
         <Grid container className={classes.grid}>
-            <Grid item xs={6} className={classes.left}>
-                <Typography component='div' className={classes.newCar}>
-                    <TextField id="standard-basic" label="Car Number" value={carNumber} onChange={(e) => setCarNumber(e.target.value.toUpperCase())} />
-                    <Button variant="contained" color="primary" disabled={carNumber ? false : true} onClick={() => addNewCar()}>
-                        Add Car
-                    </Button>
-                </Typography>
+            <Grid item xs={3} className={classes.left}>
+                {
+                    hideSlot ?
+                        <Typography component='div' className={classes.newCar}>
+                            <TextField type="number" id="standard-basic" label="Create Parking Slot" value={slot} onChange={(e) => setSlot(parseInt(e.target.value))} />
+                            <Button variant="contained" color="primary" disabled={slot ? false : true} onClick={() => addNewSlot()}>
+                                Create Slot
+                            </Button>
+                        </Typography>
+                        :
+                        <Typography component='div' className={classes.newCar}>
+                            <TextField id="standard-basic" label="Enter Car Number" value={carNumber} onChange={(e) => setCarNumber(e.target.value.toUpperCase())} />
+                            <Button variant="contained" color="primary" disabled={carNumber ? false : true} onClick={() => addNewCar()}>
+                                Add Car
+                            </Button>
+                        </Typography>
+                }
                 {
                     message ?
                         <Typography component='div' className={classes.message}>
@@ -192,31 +180,49 @@ const Home = () => {
                         </Typography>
                         : ""
                 }
-            </Grid>
-            <Grid item xs={6} className={classes.right}>
-                <Typography component='div' className={classes.bocContainer}>
-                    {
-                        store.map((item, i) => {
-                            return <Typography component='div' onClick={() => handleClick(item.id)} className={classes.box} key={i}>
-                                {item.car}
-                            </Typography>
-                        })
-                    }
-                </Typography>
                 {
-                    exit ?
+                    amountPopup ?
                         <Typography component='div' className={classes.payMoney}>
-                            <Typography component="p">Please Submit Parking Fee - ${amount}</Typography>
-                            <TextField type="number" id="standard-basic" className={classes.form} label="Car Number" value={payment} onChange={(e) => setPayment(parseInt(e.target.value))} />
-                            {
-                                user ? <Typography component="p" className={classes.incorrect}>Please Submit the correct amount!</Typography> : ""
-                            }
-                            <Button variant="contained" className={classes.form} color="primary" disabled={payment ? false : true} onClick={() => exitParking()}>
-                                Submit Payment
+                            <Typography component="p">Please Enter Total Hours</Typography>
+                            <TextField type="number" id="standard-basic" className={classes.form} label="Total Hours" value={hour} onChange={(e) => setHour(parseInt(e.target.value))} />
+                            <Button variant="contained" className={classes.form} color="primary" disabled={hour ? false : true} onClick={() => getTotalHour()}>
+                                Count Hour
                             </Button>
                         </Typography>
                         : ""
                 }
+                {exit ?
+                    <Typography component='div' className={classes.payMoney}>
+                        <Typography component="p">Please Submit Parking Fee - ${amount}</Typography>
+                        <TextField type="number" id="standard-basic" className={classes.form} label="Car Number" value={payment} onChange={(e) => setPayment(parseInt(e.target.value))} />
+                        {
+                            user ? <Typography component="p" className={classes.incorrect}>Please Submit the correct amount!</Typography> : ""
+                        }
+                        <Button variant="contained" className={classes.form} color="primary" disabled={payment ? false : true} onClick={() => exitParking()}>
+                            Submit Payment
+                        </Button>
+                    </Typography>
+                    : ""
+                }
+            </Grid>
+            <Grid item xs={9} className={classes.right}>
+                {
+                    numberOfParkingSlot.map((item, i) => {
+                        return <Card variant="outlined" key={i} className={classes.card}>
+                            <CardContent>
+                                <Typography variant="h5" component="h2">
+                                    PARKING_ID0{i + 1}
+                                </Typography>
+                                <Typography color="textSecondary" id={`carCard${i}`}>
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" onClick={() => handleClick(item)}>Exit</Button>
+                            </CardActions>
+                        </Card>
+                    })
+                }
+
             </Grid>
         </Grid>
     )
